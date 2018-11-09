2e09a9af50b37d40d8792c79eaa772b454db1059
Robert <rhinh25@gmail.com>
2018-11-08 21:30:14 -0500

    Completed tutorial 2
    Went over types and the different ways golang handles variables.

--- hello.go
+++ hello.go
@@ -14,8 +14,45 @@ func randomNumber() {
 	fmt.Println("A random number: ", rand.Intn(100))
 }
 
+func addition (a float64, b float64) float64 { //variables and return type
+	return a+b+rando
+}
+
+// A constant value
+const rando float64 = 1
+
+// note that that for each return type must be defined, even if they are all the same.
+func dblShow (word string, sentence string) (string, string) {
+	return word, sentence
+}
+
 func main() {
 	sayHello()
 	randomNumber()
 	fmt.Println("While we are at it, the square root of 16 is.., ", math.Sqrt(16))
+
+	// 1. decalre varibables separately
+	/*
+	var x float64 = 10
+	var y float64 = 5
+	*/
+
+	// 2. declare variables together in one command
+	var x,y float64 = 10, 5
+
+	// 3. since types in golang don't change, you don't need to initiate variables with type
+	// NOTE: the variables are determined (if without a type) based on the value. in the case, x&y are int
+	//x, y := 10, 5
+
+
+	//Conversion of types
+	//var x float32 = 10
+	//var y float64 = float64(x)
+
+	//OR
+
+	//t := x //t will be type x
+
+	fmt.Println(dblShow("hello","dd"))
+	fmt.Println(addition(x, y))
 }
