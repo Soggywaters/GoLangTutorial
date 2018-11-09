0d10afd15f192a6b3bb9bdacd6e2a8370212faaf
Robert <rhinh25@gmail.com>
2018-11-08 21:14:45 -0500

    Finished tutorial one
    Got done with tutorial one. Main understanding package imports, manuals
    and formatting.

--- hello.go
+++ hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func sayHello() {
+	fmt.Println("Hello world!")
+}
+
+func randomNumber() {
+	fmt.Println("A random number: ", rand.Intn(100))
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	sayHello()
+	randomNumber()
+	fmt.Println("While we are at it, the square root of 16 is.., ", math.Sqrt(16))
 }
